43483f9403946ab00a5b1d02888b76214906f378
204g1a0597 <113675125+204g1a0597@users.noreply.github.com>
2022-09-16 15:21:45 +0530

    G.sivasai Created Sample.c

---
+++ Sample.c
@@ -0,0 +1,7 @@
+# CNOSLAB
+/* Name Of the Candidate: G.sivasai
+   Roll No: 204G1A0597
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
